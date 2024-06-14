067159f452f088b97a40be6245ea60bb651f4f85
mrStefanJ <stefanjoncic92@gmail.com>
2024-06-14 12:05:15 +0200

    change style in header

--- src/components/MainNav/MainNav.tsx
+++ src/components/MainNav/MainNav.tsx
@@ -2,14 +2,12 @@ import { Link } from "react-router-dom";
 
 const MainNav = () => {
   return (
-    <>
-      <nav className="navigation__link">
-        <Link to="/">Home</Link>
-        <Link to="/tending">Tending</Link>
-        <Link to="/movies">Movies</Link>
-        <Link to="/series">Series</Link>
-      </nav>
-    </>
+    <nav className="navigation__link">
+      <Link to="/">Home</Link>
+      <Link to="/tending">Tending</Link>
+      <Link to="/movies">Movies</Link>
+      <Link to="/series">Series</Link>
+    </nav>
   );
 };
 
